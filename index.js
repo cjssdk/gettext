285fee84ef713a8431be3c06156e731a87b4f9c7
Stanislav Kalashnik <darkpark.main@gmail.com>
2016-09-30 12:38:34 +0300

    move files from spasdk/gettext repo and rework

---
+++ index.js
@@ -0,0 +1,82 @@
+/**
+ * @license The MIT License (MIT)
+ * @copyright Stanislav Kalashnik <darkpark.main@gmail.com>
+ */
+
+/* eslint no-path-concat: 0 */
+
+'use strict';
+
+
+/**
+ * Translations handler.
+ *
+ * @constructor
+ *
+ * @param {Object} config init parameters
+ */
+function Gettext ( config ) {
+    var data = config.data,
+        meta = config.meta;
+
+    /**
+     * Display the native language translation of a textual message.
+     *
+     * @param {string} msgId textual message
+     *
+     * @return {string} translated text
+     */
+    this.gettext = function ( msgId ) {
+        return data && data[''][msgId] ? data[''][msgId] : msgId;
+    };
+
+
+    /**
+     * The "p" in "pgettext" stands for "particular": fetches a particular translation of the textual message.
+     *
+     * @param {string} context message context
+     * @param {string} msgId textual message
+     *
+     * @return {string} translated text
+     */
+    this.pgettext = function ( context, msgId ) {
+        return data && data[context][msgId] ? data[context][msgId] : msgId;
+    };
+
+
+    /**
+     * Display the native language translation of a textual message whose grammatical form depends on a number.
+     *
+     * @param {string} msgId textual message in a singular form
+     * @param {string} plural textual message in a plural form
+     * @param {number} value message number
+     *
+     * @return {string} translated text
+     */
+    this.ngettext = function ( msgId, plural, value ) {
+        /* eslint no-unused-vars: 0 */
+        /* eslint no-eval: 0 */
+        /* eslint id-length: 0 */
+        var n;
+
+        if ( DEVELOP ) {
+            if ( Number(value) !== value ) { throw new Error(__filename + ': value must be a number'); }
+        }
+
+        if ( data && meta ) {
+            // translation
+            return data[''][msgId][eval('n = ' + value + '; ' + meta.plural)];
+        }
+
+        // english
+        return value === 1 ? msgId : plural;
+    };
+}
+
+
+// correct constructor name
+Gettext.prototype.constructor = Gettext;
+
+
+// public
+module.exports = Gettext;
---
+++ readme.md
@@ -0,0 +1,62 @@
+STB SDK localization
+====================
+
+[![Build Status](https://img.shields.io/travis/cjssdk/gettext.svg?style=flat-square)](https://travis-ci.org/cjssdk/gettext)
+[![NPM version](https://img.shields.io/npm/v/cjs-gettext.svg?style=flat-square)](https://www.npmjs.com/package/cjs-gettext)
+[![Dependencies Status](https://img.shields.io/david/cjssdk/gettext.svg?style=flat-square)](https://david-dm.org/cjssdk/gettext)
+[![Gitter](https://img.shields.io/badge/gitter-join%20chat-blue.svg?style=flat-square)](https://gitter.im/DarkPark/cjssdk)
+
+
+This localization module is an instance of [Emitter](https://github.com/cjssdk/emitter) module.
+Contains localization messages data used by other modules and provides methods to requests these messages.
+
+
+## Installation ##
+
+```bash
+npm install cjs-gettext
+```
+
+
+## Usage ##
+
+Add constructor to the scope and create an instance with some data:
+
+```js
+var Gettext = require('cjs-gettext'),
+    gettext = new Gettext(data);
+```
+
+Then it's possible to make direct calls to make simple replacement:
+
+```js
+console.log(gettext.gettext('some line to be localized'));
+```
+
+Fetch a particular translation of the textual message:
+
+```js
+console.log(gettext.pgettext('some context', 'some text'));
+```
+
+Native language translation of a textual message whose grammatical form depends on a number:
+
+```js
+console.log(gettext.ngettext('{0} cat', '{0} cats', 1));
+```
+
+
+## Development mode ##
+
+> There is a global var `DEVELOP` which activates additional consistency checks and protection logic not available in release mode.
+
+
+## Contribution ##
+
+If you have any problem or suggestion please open an issue [here](https://github.com/cjssdk/gettext/issues).
+Pull requests are welcomed with respect to the [JavaScript Code Style](https://github.com/DarkPark/jscs).
+
+
+## License ##
+
+`cjs-gettext` is released under the [MIT License](license.md).
